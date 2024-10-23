7a27b26afd5968c6a0fe2b625cb2d5d6099ba93a
bruno-f-cruz <7049351+bruno-f-cruz@users.noreply.github.com>
2024-10-23 14:26:19 -0700

    Refactor Services class into ServicesBuilderManager

--- src/aind_behavior_experiment_launcher/services.py
+++ src/aind_behavior_experiment_launcher/services.py
@@ -1,61 +1,92 @@
-import logging
-from typing import Optional, TypeVar, Union
+from __future__ import annotations
 
-from .apps import app_service
-from .data_mappers import data_mapper_service
-from .data_transfer import data_transfer_service
-from .resource_monitor import resource_monitor_service
+import logging
+from typing import Any, Callable, Dict, Generic, Iterable, List, Optional, Self, Type, TypeVar, overload
+
+from ._base import IService
+from .launcher import Launcher
 
 logger = logging.getLogger(__name__)
 
-SupportedServices = Union[
-    data_transfer_service.DataTransferService,
-    resource_monitor_service.ResourceMonitor,
-    app_service.BonsaiApp,
-    data_mapper_service.DataMapperService,
-]
-
-TService = TypeVar("TService", bound=SupportedServices)
+TService = TypeVar("TService", bound=IService)
 
 
-class Services:
-    # todo: this could be made more generic and to allow extension/scoping of services
-    _data_transfer: Optional[data_transfer_service.DataTransferService]
-    _resource_monitor: Optional[resource_monitor_service.ResourceMonitor]
-    _app: Optional[app_service.BonsaiApp]
-    _data_mapper: Optional[data_mapper_service.DataMapperService]
+class ServiceBuilder(Generic[TService]):
+    @overload
+    def __init__(self, service_or_builder: TService) -> None: ...
+
+    @overload
+    def __init__(self, service_or_builder: Callable[[Launcher], TService]) -> None: ...
+
+    def __init__(self, service_or_builder: Callable[[Launcher], TService] | TService) -> None:
+        self._service_builder: Optional[Callable[[Launcher], TService]] = None
+        self._service: Optional[TService] = None
+        if callable(service_or_builder):
+            self._service_builder = service_or_builder
+            self._service = None
+        elif isinstance(service_or_builder, IService):
+            self._service = service_or_builder
+            self._service_builder = None
+        else:
+            raise ValueError("service_or_builder must be either a service or a service builder")
+
+    def build(self, launcher: Launcher, *args, **kwargs) -> TService:
+        if self._service is None:
+            if self._service_builder is None:
+                raise ValueError("Service builder is not set")
+            else:
+                self._service = self._service_builder(launcher, *args, **kwargs)
+        return self._service
+
+    @property
+    def service(self) -> Optional[TService]:
+        return self._service
+
+
+class ServicesBuilderManager:
+    _services: Dict[str, ServiceBuilder]
 
     def __init__(
         self,
+        launcher: Optional[Launcher] = None,
         *args,
-        data_transfer: Optional[data_transfer_service.DataTransferService] = None,
-        resource_monitor: Optional[resource_monitor_service.ResourceMonitor] = None,
-        app: Optional[app_service.BonsaiApp] = None,
-        data_mapper: Optional[data_mapper_service.DataMapperService] = None,
         **kwargs,
     ) -> None:
-        self._data_transfer = data_transfer
-        self._resource_monitor = resource_monitor
-        self._app = app
-        self._data_mapper = data_mapper
+        self._launcher_reference = launcher
+
+    def __getitem__(self, name: str) -> IService:
+        return self._services[name].build(self.launcher)
+
+    def try_get_service(self, name: str) -> Optional[IService]:
+        return self._services[name].build(self.launcher)
+
+    def add_service(self, name: str, service_builder: ServiceBuilder) -> Self:
+        self._services[name] = service_builder
+        return self
+
+    def remove_service(self, name: str) -> Self:
+        self._services.pop(name)
+        return self
+
+    def register_launcher(self, launcher: Launcher) -> Self:
+        if self._launcher_reference is None:
+            self._launcher_reference = launcher
+        else:
+            raise ValueError("Launcher is already registered")
+        return self
 
     @property
-    def data_transfer(self) -> Optional[data_transfer_service.DataTransferService]:
-        return self._data_transfer
+    def launcher(self) -> Launcher:
+        if self._launcher_reference is None:
+            raise ValueError("Launcher is not registered")
+        return self._launcher_reference
 
     @property
-    def resource_monitor(self) -> Optional[resource_monitor_service.ResourceMonitor]:
-        return self._resource_monitor
+    def services(self) -> Iterable[IService]:
+        yield from (service.build(self.launcher) for service in self._services.values())
 
-    @property
-    def app(self) -> Optional[app_service.BonsaiApp]:
-        return self._app
+    def get_services_of_type(self, service_type: Type[TService]) -> Iterable[TService]:
+        yield from (service for service in self.services if isinstance(service, service_type))
 
-    @property
-    def data_mapper(self) -> Optional[data_mapper_service.DataMapperService]:
-        return self._data_mapper
-
-    def validate_service(self, obj: Optional[TService]) -> bool:
-        if obj is None:
-            raise ValueError("Service not set")
-        return obj.validate()
+    def map(self, delegate: Callable[[IService], Any]) -> List[Any]:
+        return [delegate(service) for service in self.services]
