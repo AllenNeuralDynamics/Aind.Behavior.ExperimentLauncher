02b4d500912f1f38ea47a9fe6a863c83289fb486
bruno-f-cruz <7049351+bruno-f-cruz@users.noreply.github.com>
2025-03-09 12:01:39 -0700

    Centralize ui tests in single file

--- tests/test_ui.py
+++ tests/test_ui.py
@@ -10,7 +10,7 @@ from aind_behavior_experiment_launcher.ui import DefaultUIHelper
 from tests import suppress_stdout
 
 
-class TestDefaultPicker(unittest.TestCase):
+class TestDefaultBehaviorPicker(unittest.TestCase):
     def setUp(self):
         self.services_factory_manager = create_autospec(BehaviorServicesFactoryManager)
         self.launcher = BehaviorLauncher(
@@ -77,6 +77,32 @@ class TestDefaultPicker(unittest.TestCase):
             mock_model_from_json_file.return_value = MagicMock()
             task_logic = self.picker.pick_task_logic()
             self.assertIsNotNone(task_logic)
+
+
+class TestDefaultUiHelper(unittest.TestCase):
+    def setUp(self):
+        self.ui_helper = DefaultUIHelper(print_func=MagicMock())
+
+    @patch("builtins.input", side_effect=["Some notes"])
+    def test_prompt_get_text(self, mock_input):
+        result = self.ui_helper.prompt_text("")
+        self.assertIsInstance(result, str)
+
+    @patch("builtins.input", side_effect=["Y"])
+    def test_prompt_yes_no_question(self, mock_input):
+        result = self.ui_helper.prompt_yes_no_question("Continue?")
+        self.assertIsInstance(result, bool)
+
+    @patch("builtins.input", side_effect=["1"])
+    def test_prompt_pick_from_list(self, mock_input):
+        result = self.ui_helper.prompt_pick_from_list(["item1", "item2"], "Choose an item")
+        self.assertIsInstance(result, str)
+        self.assertEqual(result, "item1")
+
+    @patch("builtins.input", side_effect=["0"])
+    def test_prompt_pick_from_list_none(self, mock_input):
+        result = self.ui_helper.prompt_pick_from_list(["item1", "item2"], "Choose an item")
+        self.assertIsNone(result)
 
 
 if __name__ == "__main__":
