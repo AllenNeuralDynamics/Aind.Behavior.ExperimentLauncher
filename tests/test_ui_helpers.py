9e076cc687328e89c1f8db33af23b5a59b028507
bruno-f-cruz <7049351+bruno-f-cruz@users.noreply.github.com>
2025-03-08 17:31:08 -0800

    Add tests for core interface

--- tests/test_ui_helpers.py
+++ tests/test_ui_helpers.py
@@ -1,52 +1,31 @@
 import unittest
 from unittest.mock import MagicMock, patch
-
 from aind_behavior_experiment_launcher.ui_helper import DefaultUIHelper
-
 
 class TestDefaultUiHelper(unittest.TestCase):
     def setUp(self):
         self.ui_helper = DefaultUIHelper(print_func=MagicMock())
 
-    @patch("builtins.input", side_effect=["1"])
-    def test_prompt_pick_file_from_list(self, mock_input):
-        files = ["file1.txt", "file2.txt"]
-        result = self.ui_helper.prompt_pick_file_from_list(files)
-        self.assertEqual(result, "file1.txt")
-
-    @patch("builtins.input", side_effect=["0", "manual_entry"])
-    def test_prompt_pick_file_from_list_manual_entry(self, mock_input):
-        files = ["file1.txt", "file2.txt"]
-        result = self.ui_helper.prompt_pick_file_from_list(files, zero_label="Manual Entry", zero_as_input=True)
-        self.assertEqual(result, "manual_entry")
+    @patch("builtins.input", side_effect=["Some notes"])
+    def test_prompt_get_text(self, mock_input):
+        result = self.ui_helper.prompt_text("")
+        self.assertIsInstance(result, str)
 
     @patch("builtins.input", side_effect=["Y"])
-    def test_prompt_yes_no_question_yes(self, mock_input):
+    def test_prompt_yes_no_question(self, mock_input):
         result = self.ui_helper.prompt_yes_no_question("Continue?")
-        self.assertTrue(result)
-
-    @patch("builtins.input", side_effect=["N"])
-    def test_prompt_yes_no_question_no(self, mock_input):
-        result = self.ui_helper.prompt_yes_no_question("Continue?")
-        self.assertFalse(result)
-
-    @patch("os.path.isdir", return_value=True)
-    @patch("os.listdir", return_value=["subjects/subject1", "subjects/subject2"])
+        self.assertIsInstance(result, bool)
+        
     @patch("builtins.input", side_effect=["1"])
-    def test_choose_subject(self, mock_input, mock_listdir, mock_isdir):
-        result = self.ui_helper.choose_subject("")
-        self.assertEqual(result, "subject1")
-
-    @patch("builtins.input", side_effect=["John Doe"])
-    def test_prompt_experimenter(self, mock_input):
-        result = self.ui_helper.prompt_experimenter()
-        self.assertEqual(result, ["John", "Doe"])
-
-    @patch("builtins.input", side_effect=["Some notes"])
-    def test_prompt_get_notes(self, mock_input):
-        result = self.ui_helper.prompt_text("")
-        self.assertEqual(result, "Some notes")
-
+    def test_prompt_pick_from_list(self, mock_input):
+        result = self.ui_helper.prompt_pick_from_list(["item1", "item2"], "Choose an item")
+        self.assertIsInstance(result, str)
+        self.assertEqual(result, "item1")
+    
+    @patch("builtins.input", side_effect=["0"])
+    def test_prompt_pick_from_list_none(self, mock_input):
+        result = self.ui_helper.prompt_pick_from_list(["item1", "item2"], "Choose an item")
+        self.assertIsNone(result)
 
 if __name__ == "__main__":
     unittest.main()
