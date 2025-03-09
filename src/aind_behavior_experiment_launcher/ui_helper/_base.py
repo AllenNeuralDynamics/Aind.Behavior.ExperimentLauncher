50447b4499b2d093582824be44dc42b40e3a0959
bruno-f-cruz <7049351+bruno-f-cruz@users.noreply.github.com>
2025-03-08 17:21:00 -0800

    Add input-like callable reference to UiHelper

--- src/aind_behavior_experiment_launcher/ui_helper/_base.py
+++ src/aind_behavior_experiment_launcher/ui_helper/_base.py
@@ -5,18 +5,25 @@ from typing import Any, Callable, List, Optional, TypeVar
 logger = logging.getLogger(__name__)
 
 _PrintFunc = Callable[[str], Any]
+_InputFunc = Callable[[str], str]
 _DEFAULT_PRINT_FUNC: _PrintFunc = print
+_DEFAULT_INPUT_FUNC: _InputFunc = input
 T = TypeVar("T", bound=Any)
 
 
 class UiHelperBase(abc.ABC):
     _print: _PrintFunc
+    _input: _InputFunc
 
-    def __init__(self, *args, print_func: Optional[_PrintFunc] = None, **kwargs):
+    def __init__(self, *args, print_func: Optional[_PrintFunc] = None, input_func: Optional[_PrintFunc] = None, **kwargs):
         self._print = print_func if print_func is not None else _DEFAULT_PRINT_FUNC
+        self._input = input_func if input_func is not None else _DEFAULT_INPUT_FUNC
 
     def print(self, message: str) -> Any:
         return self._print(message)
+    
+    def input(self, prompt: str) -> str:
+        return self._input(prompt)
 
     @abc.abstractmethod
     def prompt_pick_from_list(self, value: List[str], prompt: str, **kwargs) -> Optional[str]: ...
