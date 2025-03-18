2d6b8a5626ca45383a199380d920f9bb13124a12
bruno-f-cruz <7049351+bruno-f-cruz@users.noreply.github.com>
2025-03-18 10:41:05 -0700

    Improve top-level import syntax

--- src/aind_behavior_experiment_launcher/launcher/__init__.py
+++ src/aind_behavior_experiment_launcher/launcher/__init__.py
@@ -1,5 +1,12 @@
-from ._base import BaseLauncher as BaseLauncher
-from ._base import TModel as TModel
-from ._base import TRig as TRig
-from ._base import TSession as TSession
-from ._base import TTaskLogic as TTaskLogic
+from ._base import BaseLauncher, TModel, TRig, TSession, TTaskLogic
+from .cli import BaseCliArgs, run_clabe_cli
+
+__all__ = [
+    "BaseLauncher",
+    "TModel",
+    "TRig",
+    "TSession",
+    "TTaskLogic",
+    "BaseCliArgs",
+    "run_clabe_cli",
+]
